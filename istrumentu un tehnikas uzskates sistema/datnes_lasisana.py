7276d4a0b37d9cc1562610110eee13b7bab29374
Aleksandr Zelukin <alexandr44@inbox.lv>
2022-04-03 19:32:51 +0300

    Add Apps

--- istrumentu un tehnikas uzskates sistema/datnes_lasisana.py
+++ istrumentu un tehnikas uzskates sistema/datnes_lasisana.py
@@ -1,4 +1,4 @@
-
+'''
 import fileinput
 with fileinput.FileInput(files=('prieksmeti.txt', 'nomnieki.txt', 'noma.txt'), mode='r') as datne:
 
@@ -7,5 +7,35 @@ with fileinput.FileInput(files=('prieksmeti.txt', 'nomnieki.txt', 'noma.txt'), m
             
             print(f'Lasa datni {datne.filename()}...')
 
-            print(idx, line)             
+            print(idx, line)
+
+'''
+
+file1 = open('prieksmeti.txt')
+print(file1.read())
+
+file1 = open('prieksmeti.txt','w')
+
+file1.write('perforators\n')
+file1.write('slipmasina\n')
+file1.write('urbjmasinan\n')
+
+file1 = open('prieksmeti.txt')
+print(file1.read())
+
+file1.close()
                         
+file2 = open('nomnieki.txt')
+print(file2.read())
+
+file2 = open('nomnieki.txt','w')
+
+file2.write('Anna\n')
+file2.write('Valdis\n')
+file2.write('Olgerts\n')
+
+file2 = open('nomnieki.txt')
+print(file2.read())
+
+file2.close()
+                  
